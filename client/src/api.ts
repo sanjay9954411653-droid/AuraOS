927b5023c50bb4d90148e1fe6f72a1886c860db4
sanjay9954411653-droid <sanjay9954411653@gmail.com>
2026-09-18 15:59:30 -0700

    Update api.ts

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,7 +2,7 @@
 import axios, { AxiosError } from 'axios'
 
 const api = axios.create({
-  baseURL: '/api/v1',
+     baseURL: import.meta.env.VITE_API_URL || '/api/v1',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json'
